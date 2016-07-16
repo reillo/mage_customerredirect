1d6c89c8a8e9e2e3b27c27b11c196424ee39f12f
Ner Atillo <atilloregner@gmail.com>
2016-07-16 17:50:18 +0800

    redirect improvement and stop if sid is disabled

--- README.md
+++ README.md
@@ -3,7 +3,16 @@
 Magento (1.9.x) - Redirect customer group to specific store url after login.
 
 
+## Requirements
+
+Enable the `Use SID on Frontend`:
+
+```System | Configuration | Web | Session Validation Settings | Use SID on Frontend```
+
+
 ## Setup
+
+To enable the redirect:
 
 ```System | Configuration | Customers | Customer Configuration | Redirect Customer Group After Login```
 
--- app/code/local/Reillo/Customerredirect/Model/Observer.php
+++ app/code/local/Reillo/Customerredirect/Model/Observer.php
@@ -2,34 +2,47 @@
 
 class Reillo_Customerredirect_Model_Observer
 {
-    public function hookIntoCustomerLogin($observer) {
-        // run this.
-	    /**@var $customer Mage_Customer_Model_Customer*/
-	    $customer = $observer->getEvent()->getCustomer();
-	    $redirects = unserialize(Mage::getStoreConfig('customer/customerredirect/group_redirect'));
+	public function hookIntoCustomerLogin($observer) {
+		// run this.
+		/**@var $customer Mage_Customer_Model_Customer*/
+		$customer = $observer->getEvent()->getCustomer();
+		$redirects = unserialize(Mage::getStoreConfig('customer/customerredirect/group_redirect'));
+		$session = Mage::getSingleton('customer/session');
 
-	    try {
-		    $group_id = $customer->getGroupId();
-		    foreach ($redirects as $redirect) {
-			    if ($redirect['customer_group'] == $group_id) {
-				    // do not redirect if store matched current store.
-				    if ($redirect['store'] != Mage::app()->getStore()->getId()) {
-					    // generate url
-					    $uri = isset($redirect['uri_path']) ? $redirect['uri_path'] : '/';
-					    $url = Mage::helper('reillo_customerredirect')->generateUrlRedirect($redirect['store'], $uri);
+		// sid should be enabled
+		if (!$session->useSid()) {
 
-					    // redirect
-					    Mage::app()->getFrontController()->getResponse()->setRedirect($url, 302);
-					    Mage::app()->getResponse()->sendResponse();
-					    exit;
-				    }
+			// log if redirects is set and
+			if (!empty($redirects)) {
+				Mage::log('Customer redirect module requires `Use SID on Frontend` from `System | Configuration | Web` to be enabled.', Zend_Log::INFO);
+			}
 
-				    // stop for first matched customer group
-				    break;
-			    }
-		    }
-	    } catch (Exception $e) {
-		    Mage::logException($e);
-	    }
-    }
+			return ;
+		}
+
+		try {
+			$group_id = $customer->getGroupId();
+
+			if (!empty($redirects)) {
+				foreach ($redirects as $redirect) {
+					if ($redirect['customer_group'] == $group_id) {
+						// do not redirect if store matched current store.
+						if ($redirect['store'] != Mage::app()->getStore()->getId()) {
+							// generate url
+							$uri = isset($redirect['uri_path']) ? $redirect['uri_path'] : '/';
+							$url = Mage::helper('reillo_customerredirect')->generateUrlRedirect($redirect['store'], $uri);
+
+							// redirect
+							$session->setAfterAuthUrl($url);
+						}
+
+						// stop for first matched customer group
+						break;
+					}
+				}
+			}
+		} catch (Exception $e) {
+			Mage::logException($e);
+		}
+	}
 }
